47efb9f35fbe609eec713e83193f34240526652c
VienDu <phucnguyen12112@gmail.com>
2023-10-28 12:36:37 +0700

    ex2

--- Lab2_Exercises/Core/Src/main.c
+++ Lab2_Exercises/Core/Src/main.c
@@ -22,6 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "software_timer.h"
 
 /* USER CODE END Includes */
 
@@ -40,6 +41,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN PV */
 
@@ -47,6 +49,8 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -54,6 +58,102 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+void display7SEG(int num){
+  switch(num){
+	case 0:
+	  HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_C_GPIO_Port, LED_C_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_D_GPIO_Port, LED_D_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_E_GPIO_Port, LED_E_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, SET);
+	  break;
+	case 1:
+	  HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, SET);
+	  HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_C_GPIO_Port, LED_C_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_D_GPIO_Port, LED_D_Pin, SET);
+	  HAL_GPIO_WritePin(LED_E_GPIO_Port, LED_E_Pin, SET);
+	  HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, SET);
+	  HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, SET);
+	  break;
+	case 2:
+	  HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_C_GPIO_Port, LED_C_Pin, SET);
+	  HAL_GPIO_WritePin(LED_D_GPIO_Port, LED_D_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_E_GPIO_Port, LED_E_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, SET);
+	  HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, RESET);
+	  break;
+	case 3:
+	  HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_C_GPIO_Port, LED_C_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_D_GPIO_Port, LED_D_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_E_GPIO_Port, LED_E_Pin, SET);
+	  HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, SET);
+	  HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, RESET);
+	  break;
+	case 4:
+	  HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, SET);
+	  HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_C_GPIO_Port, LED_C_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_D_GPIO_Port, LED_D_Pin, SET);
+	  HAL_GPIO_WritePin(LED_E_GPIO_Port, LED_E_Pin, SET);
+	  HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, RESET);
+	  break;
+	case 5:
+	  HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, SET);
+	  HAL_GPIO_WritePin(LED_C_GPIO_Port, LED_C_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_D_GPIO_Port, LED_D_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_E_GPIO_Port, LED_E_Pin, SET);
+	  HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, RESET);
+	  break;
+	case 6:
+	  HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, SET);
+	  HAL_GPIO_WritePin(LED_C_GPIO_Port, LED_C_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_D_GPIO_Port, LED_D_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_E_GPIO_Port, LED_E_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, RESET);
+	  break;
+	case 7:
+	  HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_C_GPIO_Port, LED_C_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_D_GPIO_Port, LED_D_Pin, SET);
+	  HAL_GPIO_WritePin(LED_E_GPIO_Port, LED_E_Pin, SET);
+	  HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, SET);
+	  HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, SET);
+	  break;
+	case 8:
+	  HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_C_GPIO_Port, LED_C_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_D_GPIO_Port, LED_D_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_E_GPIO_Port, LED_E_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, RESET);
+	  break;
+	case 9:
+	  HAL_GPIO_WritePin(LED_A_GPIO_Port, LED_A_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_C_GPIO_Port, LED_C_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_D_GPIO_Port, LED_D_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_E_GPIO_Port, LED_E_Pin, SET);
+	  HAL_GPIO_WritePin(LED_F_GPIO_Port, LED_F_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, RESET);
+	  break;
+	default:
+	  break;
+  }
+}
 /* USER CODE END 0 */
 
 /**
@@ -72,7 +172,9 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
+  setTimer1(50);
+  setTimer2(100);
+  int num = 1;
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -83,18 +185,72 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_TIM_Base_Start_IT(&htim2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
+	while (1)
+	{
+	  /* USER CODE END WHILE */
+	  if (timer2_flag == 1){
+		  HAL_GPIO_TogglePin(GPIOA, LED_RED_Pin);
+		  HAL_GPIO_TogglePin(GPIOA, DOT_Pin);
+		  setTimer2(100);
+	  }
+	  switch (num){
+	  case 1:
+		  display7SEG(num);
+		  if (timer1_flag == 1){
+			  HAL_GPIO_WritePin(GPIOA, EN0_Pin, SET);
+			  HAL_GPIO_WritePin(GPIOA, EN1_Pin, RESET);
+			  HAL_GPIO_WritePin(GPIOA, EN2_Pin, SET);
+			  HAL_GPIO_WritePin(GPIOA, EN3_Pin, SET);
+			  num = 2;
+			  setTimer1(50);
+		  }
+		  break;
+	  case 2:
+		  display7SEG(num);
+		  if (timer1_flag == 1){
+			  HAL_GPIO_WritePin(GPIOA, EN0_Pin, SET);
+			  HAL_GPIO_WritePin(GPIOA, EN1_Pin, SET);
+			  HAL_GPIO_WritePin(GPIOA, EN2_Pin, RESET);
+			  HAL_GPIO_WritePin(GPIOA, EN3_Pin, SET);
+			  num = 3;
+			  setTimer1(50);
+		  }
+		  break;
+	  case 3:
+		  display7SEG(num);
+		  if (timer1_flag == 1){
+			  HAL_GPIO_WritePin(GPIOA, EN0_Pin, SET);
+			  HAL_GPIO_WritePin(GPIOA, EN1_Pin, SET);
+			  HAL_GPIO_WritePin(GPIOA, EN2_Pin, SET);
+			  HAL_GPIO_WritePin(GPIOA, EN3_Pin, RESET);
+			  num = 0;
+			  setTimer1(50);
+		  }
+		  break;
+	  case 0:
+		  display7SEG(num);
+		  if (timer1_flag == 1){
+			  HAL_GPIO_WritePin(GPIOA, EN0_Pin, RESET);
+			  HAL_GPIO_WritePin(GPIOA, EN1_Pin, SET);
+			  HAL_GPIO_WritePin(GPIOA, EN2_Pin, SET);
+			  HAL_GPIO_WritePin(GPIOA, EN3_Pin, SET);
+			  num = 1;
+			  setTimer1(50);
+		  }
+		  break;
+	  default:
+		  break;
+	  }
+	  /* USER CODE BEGIN 3 */
+	}
   /* USER CODE END 3 */
 }
 
@@ -133,8 +289,97 @@ void SystemClock_Config(void)
   }
 }
 
-/* USER CODE BEGIN 4 */
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
 
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 7999;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 9;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, DOT_Pin|LED_RED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LED_A_Pin|LED_B_Pin|LED_C_Pin|LED_D_Pin
+                          |LED_E_Pin|LED_F_Pin|LED_G_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : DOT_Pin LED_RED_Pin EN0_Pin EN1_Pin
+                           EN2_Pin EN3_Pin */
+  GPIO_InitStruct.Pin = DOT_Pin|LED_RED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LED_A_Pin LED_B_Pin LED_C_Pin LED_D_Pin
+                           LED_E_Pin LED_F_Pin LED_G_Pin */
+  GPIO_InitStruct.Pin = LED_A_Pin|LED_B_Pin|LED_C_Pin|LED_D_Pin
+                          |LED_E_Pin|LED_F_Pin|LED_G_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim){
+	timerRun();
+
+}
 /* USER CODE END 4 */
 
 /**
